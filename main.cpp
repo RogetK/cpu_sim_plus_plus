9f34b49f5d908f6cbd5bca56345a15b011a5fdd8
Roget Kou <roget@rkou.cc>
2019-10-31 22:57:29 +0000

    added files

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <cpu_types>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "Hello World\n";
+    return 0;
+}
